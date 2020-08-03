ded10c56cb40c6c2facbda31de3f29b31b3ebce2
bomee <primavera0331@gmail.com>
2020-08-03 23:30:40 +0900

    merge sort

---
+++ 11_mergeSort.js
@@ -0,0 +1,32 @@
+const log = console.log;
+
+function mergeSort(input) {
+  const arr = input.slice();
+  if (arr.length <= 1) return arr;
+  const mid = Math.floor(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  const result = [];
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      result.push(left.shift());
+    } else {
+      result.push(right.shift());
+    }
+  }
+  while (left.length) {
+    result.push(left.shift());
+  }
+  while (right.length) {
+    result.push(right.shift());
+  }
+  return result;
+}
+
+const input = "10 20 30 5 1".split(" ").map((n) => parseInt(n, 10));
+
+console.log(mergeSort(input));
